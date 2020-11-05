9621f4a3e86ccbbae1e5a22d17620df87fe84c91
Vyshnavi2000 <m.n.vyshnavi06@gmail.com>
2020-11-05 14:48:54 +0530

    first commit

---
+++ lib/screens/settings_screen.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
+
+class SettingsScreen extends StatefulWidget {
+  static const routeName = '/settings';
+  final Function saveFilters;
+  final Map<String, bool> currentFilters;
+
+  SettingsScreen(this.currentFilters, this.saveFilters);
+
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _glutenFree = false;
+  bool _vegan = false;
+  bool _vegetarian = false;
+  bool _lactoseFree = false;
+
+  @override
+  initState() {
+    _glutenFree = widget.currentFilters['gluten'];
+    _lactoseFree = widget.currentFilters['lactose'];
+    _vegan = widget.currentFilters['vegan'];
+    _vegetarian = widget.currentFilters['vegatarian'];
+    super.initState();
+  }
+
+  Widget _buildSwitchTileList(
+    String title,
+    String description,
+    bool currentValue,
+    Function updateValue,
+  ) {
+    return SwitchListTile(
+      title: Text(title),
+      subtitle: Text(description),
+      value: currentValue,
+      onChanged: updateValue,
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Settings'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              final selectedFilters = {
+                'gluten': _glutenFree,
+                'lactose': _lactoseFree,
+                'vegan': _vegan,
+                'vegetarian': _vegetarian,
+              };
+              widget.saveFilters(selectedFilters);
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            child: Text(
+              'Adjust your meal selection.',
+              style: Theme.of(context).textTheme.headline1,
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: [
+                _buildSwitchTileList(
+                  'Gluten-Free',
+                  'Only incude gluten-free meals',
+                  _glutenFree,
+                  (newValue) {
+                    setState(
+                      () {
+                        _glutenFree = newValue;
+                      },
+                    );
+                  },
+                ),
+                _buildSwitchTileList(
+                  'Vegan',
+                  'Only incude vegan meals',
+                  _vegan,
+                  (newValue) {
+                    setState(() {
+                      _vegan = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchTileList(
+                  'Vegetarian',
+                  'Only incude vegetarian meals',
+                  _vegetarian,
+                  (newValue) {
+                    setState(() {
+                      _vegetarian = newValue;
+                    });
+                  },
+                ),
+                _buildSwitchTileList(
+                  'Lactose-Free',
+                  'Only incude lactose-free meals',
+                  _lactoseFree,
+                  (newValue) {
+                    setState(() {
+                      _lactoseFree = newValue;
+                    });
+                  },
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+      drawer: MainDrawer(),
+    );
+  }
+}
---
+++ lib/screens/tabs_screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+import '../widgets/main_drawer.dart';
+import 'categories_screen.dart';
+import 'favorites_screen.dart';
+
+class TabScreen extends StatefulWidget {
+  static const routeName = '/tabs_screen';
+  @override
+  _TabScreenState createState() => _TabScreenState();
+}
+
+class _TabScreenState extends State<TabScreen> {
+  final List<Map<String, Object>> _pages = [
+    {
+      'page': CategoriesScreen(),
+      'title': 'Categories',
+    },
+    {
+      'page': Favorites(),
+      'title': 'Your Favorites',
+    },
+  ];
+  int _selectedPageIndex = 0;
+  void _selectPage(int index) {
+    setState(() {
+      _selectedPageIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_pages[_selectedPageIndex]['title']),
+      ),
+      drawer: MainDrawer(),
+      body: _pages[_selectedPageIndex]['page'],
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Theme.of(context).primaryColor,
+        currentIndex: _selectedPageIndex,
+        unselectedItemColor: Colors.white,
+        selectedItemColor: Theme.of(context).accentColor,
+        onTap: _selectPage,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.star),
+            label: 'Favorites',
+          ),
+        ],
+      ),
+    );
+    // return DefaultTabController(
+    //   length: 2,
+    //   child: Scaffold(
+    //     appBar: AppBar(
+    //       title: Text('Meals'),
+    //       bottom: TabBar(
+    //         tabs: [
+    //           Tab(
+    //             icon: Icon(
+    //               Icons.category,
+    //             ),
+    //             text: 'Categories',
+    //           ),
+    //           Tab(
+    //             icon: Icon(Icons.star),
+    //             text: 'Favorites',
+    //           ),
+    //         ],
+    //       ),
+    //     ),
+    //     body: TabBarView(
+    //       children: [
+    //         CategoriesScreen(),
+    //         Favorites(),
+    //       ],
+    //     ),
+    //   ),
+    // );
+  }
+}
